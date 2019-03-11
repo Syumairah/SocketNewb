7e94fb71f36956242aa2b90f42d0def330659777
aimanpoji <34196370+aimanpoji@users.noreply.github.com>
2019-03-11 19:10:39 +0800

    Update client.java

--- client.java
+++ client.java
@@ -1,30 +1,33 @@
 import java.io.*;
 import java.net.*;
-
+import java.util.*;
+ 
 class Client {
-    public static void main(String[] args) throws IOException {
-
-        if (args.length != 2) {
-            System.err.println(
-                "Usage: java EchoClient <host name> <port number>");
-            System.exit(1);
-        }
-
-        String hostName = args[0];
+    
+    public static void main(String argv[])
+      {
+	    
+        try{
+            String hostName = args[0];
         int portNumber = Integer.parseInt(args[1]);
-        Socket firstSocket = new Socket(hostName, portNumber);
-        PrintWriter out = new PrintWriter(firstSocket.getOutputStream(), true);
-        BufferedReader in = new BufferedReader(new InputStreamReader(firstSocket.getInputStream()));
-        BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+            //establish socket connection to server
+            Socket socketclient = new Socket(hostName, portNumber);
+            PrintWriter out = new PrintWriter(firstSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(firstSocket.getInputStream()));
+            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+            
         String userInput;
+           writer.write("*** Hi from Client \n"); 
         while ((userInput = stdIn.readLine()) != null) 
         {
-            out.println(userInput);
             System.out.println("received: " + in.readLine());
         }
         in.close();
         stdIn.close();
         firstSocket.close();
-
-    }
+            
+           
+        }
+	  }
+ 
 }

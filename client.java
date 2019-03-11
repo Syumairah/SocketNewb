f38d2e15f21957a5174528f40c38d59dc20dbd99
aimanpoji <34196370+aimanpoji@users.noreply.github.com>
2019-03-11 20:59:10 +0800

    Update client.java

--- client.java
+++ client.java
@@ -14,13 +14,14 @@ public class SocketClientExample {
 
     public static void main(String[] args) throws UnknownHostException, IOException, ClassNotFoundException, InterruptedException{
         //get the localhost IP address, if server is running on some other IP, you need to use that
-        InetAddress host = InetAddress.getLocalHost();
+        String hostName = args[0];
+        int portNumber = Integer.parseInt(args[1]);
         Socket socket = null;
         ObjectOutputStream oos = null;
         ObjectInputStream ois = null;
         for(int i=0; i<5;i++){
             //establish socket connection to server
-            socket = new Socket(host.getHostName(), 9876);
+            socket = new Socket(hostName, portNumber);
             //write to socket using ObjectOutputStream
             oos = new ObjectOutputStream(socket.getOutputStream());
             System.out.println("Sending request to Socket Server");
